7609d6f6be04ba138a63796b6f043ddf2e90a5e6
davidzonn <davidzonneveld@gmail.com>
2016-03-26 15:23:34 +0000

    Added the web pages

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Web Tech & Mobile Apps</title>
+		<link rel = "stylesheet" type = "text/css" href = "res/css/mainLayout.css">
+		<link rel = "stylesheet" type = "text/css" href = "res/css/header.css">
+		<link rel = "stylesheet" type = "text/css" href = "res/css/footer.css">
+		<link rel = "stylesheet" type = "text/css" href = "res/css/nav.css">
+		<link rel = "stylesheet" type = "text/css" href = "res/css/main.css">
+	</head>
+	
+	<body>		
+		<?php require ('res/templates/header.html');?>
+		<?php require ('res/templates/main.html');?>
+		<?php require ('res/templates/nav.html');?>
+		<?php require ('res/templates/footer.html');?>
+		<script src="scripts/main.js"> </script>
+		<script src = "scripts/nav.js"></script>
+		<script src = "scripts/lib/fb.js"></script>
+	</body>
+</html>
